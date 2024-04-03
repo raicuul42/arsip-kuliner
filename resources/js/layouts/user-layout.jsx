74b87dd5221060340d2cefa1ef5b2196843db18c
Raisul Amin R <raicuul42@users.noreply.github.com>
2024-04-04 01:51:03 +0800

    fixing nav on user layout

--- resources/js/layouts/user-layout.jsx
+++ resources/js/layouts/user-layout.jsx
@@ -2,14 +2,14 @@ import { Aside } from '@/components/aside';
 import { Head } from '@inertiajs/react';
 import { Container } from '@/components/container';
 import { Toaster } from '@/components/ui/sonner';
-import { Nav } from './navigation/nav';
+import { Navigation } from '@/layouts/navigation/index';
 
 export function UserLayout({ title, children }) {
     return (
         <div className="flex min-h-screen w-full flex-col">
             <Toaster position="top-right" />
             <Head title={title} />
-            <Nav />
+            <Navigation />
             <main className="min-h-[calc(100vh_-_theme(spacing.16))] bg-muted/40 py-6 lg:gap-8 lg:py-16">
                 <div className="-mt-6 mb-6 border-b bg-background py-6 lg:-mt-16 lg:mb-12">
                     <Container>
