746af004e279b3cfee1587faec40834a3be8a96c
Raisul Amin R <raicuul42@users.noreply.github.com>
2024-05-07 09:42:58 +0800

    fixing footer

--- resources/js/components/footer.jsx
+++ resources/js/components/footer.jsx
@@ -94,99 +94,11 @@ const navigation = {
 
 export function Footer() {
     return (
-        <footer className="bg-background" aria-labelledby="footer-heading">
+        <footer className="mb-14 bg-background lg:mb-0" aria-labelledby="footer-heading">
             <h2 id="footer-heading" className="sr-only">
                 Footer
             </h2>
             <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
-                <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-                    <div className="space-y-8">
-                        <NewLogo />
-                        <p className="text-sm leading-6 text-muted-foreground">
-                            Terhubung dengan kekayaan budaya Nusantara melalui aplikasi kami yang mempersembahkan lebih
-                            dari sekadar resep masakan. Di sini, Anda akan menemukan cerita-cerita menarik, tips-tips
-                            memasak, dan panduan lengkap untuk mengeksplorasi dunia kuliner Nusantara.
-                        </p>
-                        <div className="flex space-x-6">
-                            {navigation.social.map((item) => (
-                                <a
-                                    key={item.name}
-                                    href={item.href}
-                                    className="text-gray-500 hover:text-muted-foreground"
-                                >
-                                    <span className="sr-only">{item.name}</span>
-                                    <item.icon className="h-6 w-6" aria-hidden="true" />
-                                </a>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-                        <div className="md:grid md:grid-cols-2 md:gap-8">
-                            <div>
-                                <h3 className="text-sm font-semibold leading-6 text-foreground">Solutions</h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    {navigation.solutions.map((item) => (
-                                        <li key={item.name}>
-                                            <a
-                                                href={item.href}
-                                                className="text-sm leading-6 text-muted-foreground hover:text-foreground"
-                                            >
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <div className="mt-10 md:mt-0">
-                                <h3 className="text-sm font-semibold leading-6 text-foreground">Support</h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    {navigation.support.map((item) => (
-                                        <li key={item.name}>
-                                            <a
-                                                href={item.href}
-                                                className="text-sm leading-6 text-muted-foreground hover:text-foreground"
-                                            >
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="md:grid md:grid-cols-2 md:gap-8">
-                            <div>
-                                <h3 className="text-sm font-semibold leading-6 text-foreground">Company</h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    {navigation.company.map((item) => (
-                                        <li key={item.name}>
-                                            <a
-                                                href={item.href}
-                                                className="text-sm leading-6 text-muted-foreground hover:text-foreground"
-                                            >
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <div className="mt-10 md:mt-0">
-                                <h3 className="text-sm font-semibold leading-6 text-foreground">Legal</h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    {navigation.legal.map((item) => (
-                                        <li key={item.name}>
-                                            <a
-                                                href={item.href}
-                                                className="text-sm leading-6 text-muted-foreground hover:text-foreground"
-                                            >
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
                     <p className="text-xs leading-5 text-muted-foreground">
                         &copy; 2024 Universitas Muhammadiyah Parepare. Arsip Kuliner.
